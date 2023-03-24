6ff63d38e8d2010ea6e91d1d50d81dc884635d39
Olivier Pagé <oliv66001@gmail.com>
2023-03-24 16:33:05 +0100

    Modification and deletion of the user account by the admin

---
+++ public/assets/js/adminusers.js
@@ -0,0 +1,34 @@
+let links = document.querySelectorAll("[data-delete]");
+
+for (let link of links) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if (confirm("Voulez-vous supprimer cet utilisateur ?")) {
+            fetch(this.getAttribute("href"), {
+                method: "DELETE",
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ "_token": this.dataset.token })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    this.parentElement.remove();
+
+                      // Afficher le message flash
+                    alert(data.message);
+                    // Redirection vers la route 'admin_dishes_index'
+                    window.location.href = '/admin/utilisateurs';
+                } else {
+                    alert(data.error);
+                }
+            })
+            .catch(error => {
+                console.error('Erreur lors de la suppression:', error);
+            });
+        }
+    });
+}
--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -9,11 +9,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/utilisateurs', name: 'admin_users_')]
 class UsersController extends AbstractController
 {
+ 
     #[Route('/', name: 'index')]
     public function index(UsersRepository $usersRepository): Response
     {
@@ -57,13 +61,44 @@ class UsersController extends AbstractController
         ]);
     }
 
-#[Route('/suppression/{id}', name: 'delete')]
-    public function delete(Users $users): Response
-    {
-        //Vérification si l'user peut supprimer avec le voter
-        $this->denyAccessUnlessGranted('USER_EDIT', $users);
+   // Ajoutez l'argument UserRepository à votre méthode deleteUser
+#[Route('/suppression/user/{id}', name: 'delete_user', methods: ['DELETE'])]
+public function deleteUser(
+    Request $request,
+    EntityManagerInterface $em,
+    int $id,
+    UsersRepository $userRepository,
+    LoggerInterface $logger // Ajoutez ceci
+): JsonResponse {
 
-        return $this->render('admin/users/index.html.twig');
+    // Récupérer l'utilisateur à supprimer
+    $userToDelete = $userRepository->find($id);
+   
+
+    if (!$userToDelete) {
+        return new JsonResponse(['error' => 'Utilisateur introuvable'], 404);
     }
+
+    $logger->info('User to delete: ' . $userToDelete->getId());
+
+    $data = json_decode($request->getContent(), true);
+
+    // On vérifie si le token est valide
+    if ($this->isCsrfTokenValid('delete_user' . $userToDelete->getId(), $data['_token'])) {
+
+       
+        // On supprime le compte utilisateur de la base
+        $em->remove($userToDelete);
+            $em->flush();
+    
+            $this->addFlash('success', 'Compte utilisateur supprimé avec succès.');
+    
+            return new JsonResponse(['success' => true, 'message' => 'Compte utilisateur supprimé avec succès'], 200);
+        }
+    
+        // Échec de la suppression
+        return new JsonResponse(['error' => 'Token invalide'], 400);
+    }
+    
 
 }
